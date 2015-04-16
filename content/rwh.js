44190877d23f2a56f2c008f12b9224641e7feab6
Jeevanandam M <jeeva@myjeeva.com>
2015-04-15 23:02:55 -0700

    Msg header reading and parsing from Msg DB

--- content/rwh.js
+++ content/rwh.js
@@ -37,7 +37,9 @@ var ReplyWithHeader = {
 
             var srcName = ex.fileName || '';
             var scriptError = Cc['@mozilla.org/scripterror;1'].createInstance(Ci.nsIScriptError);
-            scriptError.init(msg + '\n' + ex.message, srcName, stack, ex.lineNumber, 0, 0x0, group);
+
+            // Addon generates this error, it is better to use warningFlag = 0x1
+            scriptError.init(msg + '\n' + ex.message, srcName, stack, ex.lineNumber, 0, 0x1, group);
             this.service.logMessage(scriptError);
         }
     },
@@ -51,23 +53,85 @@ var ReplyWithHeader = {
         return !(typeof o === 'undefined');
     },
 
+    get isEnabled() {
+        var enabled = ReplyWithHeader.Prefs.getBool('extensions.replywithheader.enable');
+        ReplyWithHeader.Log.debug('RWH isEnabled: ' + enabled);
+
+        return enabled;
+    },
+
+    get isOkayToMoveOn() {
+        // gComposeType can be used, will try later
+        // Compose type have to be 1=Reply, 2=ReplyAll, 4=ForwardInline, 6=ReplyToSender then
+        let mct = Ci.nsIMsgCompType;
+        let ct = gMsgCompose.type;
+
+        var isOkay = (ct == mct.Reply || ct == mct.ReplyAll || mct.ReplyToSender || mct.ForwardInline);
+        ReplyWithHeader.Log.debug('isOkayToMoveOn: ' + isOkay);
+
+        return isOkay;
+    },
+
     get messageUri() {
-        if (ReplyWithHeader.isDefined(gMsgCompose.originalMsgURI)) {
-            return gMsgCompose.originalMsgURI;
+        var msgUri = null;
+
+        if (this.isDefined(gMsgCompose.originalMsgURI)) {
+            msgUri = gMsgCompose.originalMsgURI;
         } else {
             ReplyWithHeader.Log.debug('gMsgCompose.originalMsgURI is not defined, fallback');
             let selectedURIs = GetSelectedMessages();
             try {
-                return selectedURIs[0]; // only first message
+                msgUri = selectedURIs[0]; // only first message
             } catch(ex) {
                 ReplyWithHeader.Log.error('Error occurred while getting selected message.', ex);
                 return false;
             }
         }
+        ReplyWithHeader.Log.debug('Original Message URI: ' + msgUri);
+
+        return msgUri;
     },
 
     get isHtmlMail() {
         return gMsgCompose.composeHTML;
+    },
+
+    getMsgHeader: function(mUri) {
+        try {
+            // let messenger = Cc['@mozilla.org/messenger;1'].createInstance(Ci.nsIMessenger);
+            let messengerService = gMessenger.messageServiceFromURI(mUri);
+            return messengerService.messageURIToMsgHdr(mUri);
+        } catch(ex) {
+            ReplyWithHeader.Log.error('Unable to get message [' + mUri + ']', ex);
+            return null;
+        }
+    },
+
+    parseDate: function(prtime) {
+        // Input is epoch time
+        let d = new Date(prtime / 1000);
+
+        var nd = d.toString();
+        ReplyWithHeader.Log.debug('Parsed date: ' + nd);
+
+        return nd;
+    },
+
+    parseMsgHeader: function(hdr) {
+        // Decoding values into object
+        var header = { 'from': hdr.mime2DecodedAuthor,
+                     'to': hdr.mime2DecodedRecipients,
+                     'cc': hdr.ccList,
+                     'date': this.parseDate(hdr.date),
+                     'subject': hdr.mime2DecodedSubject };
+
+        ReplyWithHeader.Log.debug('\nFrom: ' + header.from
+                                + '\nTo: ' + header.to
+                                + '\nCC: '+ header.cc
+                                + '\nSubject: ' + header.subject
+                                + '\nDate: ' + header.date);
+
+        return header;
     },
 
     initListener: function() {
@@ -85,6 +149,28 @@ var ReplyWithHeader = {
     },
 
     handleMailCompose: function() {
+        let log = ReplyWithHeader.Log;
 
+        /*
+         * ReplyWithHeader has to be enabled; extensions.replywithheader.enable=true and
+         * Compose type have to be 1=Reply, 2=ReplyAll, 4=ForwardInline, 6=ReplyToSender then
+         * Addon comes into play :)
+         */
+        if (ReplyWithHeader.isEnabled && ReplyWithHeader.isOkayToMoveOn) {
+            log.debug('handleMailCompose()');
+
+            log.debug(' gComposeType==> ' +  gComposeType + '\t gMsgCompose.type==> ' + gMsgCompose.type);
+
+            //ReplyWithHeader.Log.debug(gMsgCompose.editor.rootElement.innerHTML);
+
+            log.debug('is HTML email ==> ' + this.isHtmlMail);
+            //log.debug('gCurrentIdentity.identityName==>' + gCurrentIdentity.identityName);
+
+            var rawhdr = this.getMsgHeader(this.messageUri);
+            var header = this.parseMsgHeader(rawhdr);
+
+        } else {
+            log.info('ReplyWithHeader is not enabled, also \n kindly enable it from Add-on Preferences.');
+        }
     }
 };
