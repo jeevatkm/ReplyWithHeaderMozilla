57dc2bf43e537b1fdaf2feaed187a404d2c67ef2
Jeevanandam M <jeeva@myjeeva.com>
2015-04-17 12:51:23 -0700

    RWH default prefs added

--- defaults/preferences/rwh-defaults.js
+++ defaults/preferences/rwh-defaults.js
@@ -2,12 +2,13 @@
 pref('extensions.replywithheader.enable', true);
 
 // RWH Header
-pref('extensions.replywithheader.header.from.style', true);
+pref('extensions.replywithheader.header.from.style', 0);
+pref('extensions.replywithheader.header.label.type', 1);
 pref('extensions.replywithheader.header.font', 'Tahoma');
-//pref('extensions.replywithheader.header.font.size', 13);
+pref('extensions.replywithheader.header.font.size', 13);
 
 // RWH Mail message
-
+// Upcoming things here!
 
 // RWH Debug settings
 pref('extensions.replywithheader.debug', false);
