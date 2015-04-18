fc22f6ad76038c8feb749dc6e65ff410750efb26
Jeevanandam M <jeeva@myjeeva.com>
2015-04-18 00:21:40 -0700

    Implemented initial version of RWH preferences

--- content/rwh-prefs.js
+++ content/rwh-prefs.js
@@ -32,14 +32,92 @@ ReplyWithHeader.Prefs = {
     },
 
     openWebsite: function() {
-        alert('Website');
+        this.openUrlInDefaultBrowser(ReplyWithHeader.homepageUrl);
     },
 
     openFeedback: function() {
-        alert('Feedback');
+        this.openUrlInDefaultBrowser(ReplyWithHeader.feedbackPageUrl);
     },
 
-    openSupport: function() {
-        alert('Support');
+    reportIssues: function() {
+        this.openUrlInDefaultBrowser(ReplyWithHeader.issuesPageUrl);
+    },
+
+    openPaypal: function() {
+        this.showAlert('Opening PayPal Service. Thanks for supporting ReplyWithHeader.');
+        this.openUrlInDefaultBrowser(ReplyWithHeader.paypalDonateUrl);
+    },
+
+    copyBtcAddress: function() {
+        this.copyToClipboard(ReplyWithHeader.btcAddress);
+        this.showAlert('BTC address is copied. Thanks for supporting ReplyWithHeader.');
+    },
+
+    copyToClipboard: function(str) {
+        if (str) {
+            let clipboardHelper = Cc['@mozilla.org/widget/clipboardhelper;1'].getService(Ci.nsIClipboardHelper);
+            clipboardHelper.copyString(str);
+        }
+    },
+
+    showAlert: function(str) {
+        if (str) {
+            let alertsService = Cc['@mozilla.org/alerts-service;1'].getService(Ci.nsIAlertsService);
+            try {
+                alertsService.showAlertNotification('chrome://replywithheader/skin/icon-64.png',
+                                                    ReplyWithHeader.addonName, str, false, '', null, '');
+            } catch(ex) {
+                ReplyWithHeader.Log.errorWithException('Unable to show RWH notify alert.', ex);
+            }
+        }
+    },
+
+    openUrlInDefaultBrowser: function(url) {
+        let messenger = Cc['@mozilla.org/messenger;1'].createInstance(Ci.nsIMessenger);
+        try {
+            messenger.launchExternalURL(url);
+        } catch(ex) {
+            ReplyWithHeader.Log.errorWithException('Unable to open RWH URL.', ex);
+        }
+    },
+
+    init: function() {
+        this.toggleRwh();
+
+        document.getElementById('hdrFontsize').selectedIndex = 3;
+    },
+
+    toggleRwh: function() {
+        let rwh = document.getElementById('enableRwh');
+
+        if (rwh.checked) {
+            this.toggle('lblFromAttribution', false);
+            this.toggle('fromAttributionStyle', false);
+
+            this.toggle('lblHeaderlabel', false);
+            this.toggle('toccAttributionStyle', false);
+
+            /*this.toggle('lblTypography', false);
+            this.toggle('lblFontface', false);
+            this.toggle('hdrFontface', false);
+            this.toggle('lblFontsize', false);
+            this.toggle('hdrFontsize', false); */
+        } else {
+            this.toggle('lblFromAttribution', true);
+            this.toggle('fromAttributionStyle', true);
+
+            this.toggle('lblHeaderlabel', true);
+            this.toggle('toccAttributionStyle', true);
+
+            /*this.toggle('lblTypography', true);
+            this.toggle('lblFontface', true);
+            this.toggle('hdrFontface', true);
+            this.toggle('lblFontsize', true);
+            this.toggle('hdrFontsize', true);*/
+        }
+    },
+
+    toggle: function(id, v) {
+        document.getElementById(id).disabled = v;
     }
 };
