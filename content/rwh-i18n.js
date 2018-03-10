6b6f0085ce8b28b4bf9050d82a9208df462f1de1
ofa- <ofa@github>
2018-03-10 13:32:09 +0100

    move i18n to separate file rwh-i18n.js

---
+++ content/rwh-i18n.js
@@ -0,0 +1,49 @@
+// coding: utf-8
+
+var i18n = {
+  "lang": {
+    "en": "English",
+    "fr": "French",
+    "de": "German",
+  },
+  "from": {
+    "en": "From:",
+    "fr": "De :",
+    "de": "Von:",
+  },
+  "to": {
+    "en": "To:",
+    "fr": "Pour :",
+    "de": "An:",
+  },
+  "cc": {
+    "en": "Cc:",
+    "fr": "Cc :",
+    "de": "Cc:",
+  },
+  "subject": {
+    "en": "Subject:",
+    "fr": "Objet :",
+    "de": "Betreff:",
+  },
+  "date": {
+    "en": "Date:",
+    "fr": "Date :",
+    "de": "Datum:",
+  },
+  "sent": {
+    "en": "Sent:",
+    "fr": "Envoyé :",
+    "de": "Gesendet:",
+  },
+  "forwarded_message": {
+    "en": "Forwarded Message",
+    "fr": "Message transféré",
+    "de": "Weitergeleitete Nachricht",
+  },
+  "original_message": {
+    "en": "Original Message",
+    "fr": "Message d'origine",
+    "de": "Ursprüngliche Nachricht",
+  },
+};
