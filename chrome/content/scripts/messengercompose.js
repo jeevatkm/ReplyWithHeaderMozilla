caf5b9c9775f4dfee284ee84b8a5a48269b47b8f
Jeevanandam M <jeeva@myjeeva.com>
2020-09-29 13:27:48 -0700

    GH #99 made addon work in TB 68 using window listener

---
+++ chrome/content/scripts/messengercompose.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) Jeevanandam M. (jeeva@myjeeva.com)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at
+ * https://github.com/jeevatkm/ReplyWithHeaderMozilla/blob/master/LICENSE
+ */
+
+// Import any needed modules.
+var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+// Load an additional JavaScript file.
+Services.scriptloader.loadSubScript('chrome://replywithheader/content/core.js', window, 'UTF-8');
+Services.scriptloader.loadSubScript('chrome://replywithheader/content/prefs.js', window, 'UTF-8');
+Services.scriptloader.loadSubScript('chrome://replywithheader/content/i18n.js', window, 'UTF-8');
+// Services.scriptloader.loadSubScript('chrome://replywithheader/content/rwh-prefs.js', window, 'UTF-8');
+
+function onLoad(activatedWhileWindowOpen) {
+  // Registering RWH into compose window of Thunderbird - msgcomposeWindow
+  window.addEventListener('compose-window-init', function() {
+    window.ReplyWithHeader.Prefs.fixCursorBlink();
+    window.ReplyWithHeader.init()
+  }, true);
+}
+
+function onUnload(deactivatedWhileWindowOpen) {
+
+}
