abb26fa858f3ccce7eefd1c27ad5ab7177afc331
John Bieling <john.bieling@gmx.de>
2023-07-24 18:36:51 +0200

    Use LegacyPrefs API instead of WindowListener API to set default prefs

--- scripts/background.js
+++ scripts/background.js
@@ -7,10 +7,66 @@
  * https://github.com/jeevatkm/ReplyWithHeaderMozilla/blob/master/LICENSE
  */
 
-(async () => {
+const PREF_PREFIX = "extensions.replywithheader.";
 
-  messenger.WindowListener.registerDefaultPrefs("defaults/preferences/rwh-defaults.js");
+const PREF_DEFAULTS = {
+  // RWH default preferences values
+  "enable": true,
 
+  // RWH Header
+  "header.from.style": 2,
+  "header.tocc.style": 1,
+  "header.lblseq.style": 1,
+  "header.locale": "en-US",
+  "header.font.face": "Tahoma",
+  "header.font.size": 13,
+  "header.font.size.unit": "px",
+  "header.font.color": "#000000",
+  "header.separator.space.before": 1,
+  "header.space.before": 0,
+  "header.space.after": 1,
+  "header.separator.line.size": 1,
+  "header.separator.line.color": "#B5C4DF",
+  //   "auto.select.lang": false,
+  //   "auto.select.lang.regex.list": "",
+  "trans.subject.prefix": false,
+  //   "use.sender.date": false,
+  //   "use.local.date.regex.list": "",
+
+  // RWH Date & Time
+  // 0 - Locale date format
+  // 1 - International date format - UTC
+  "header.date.format": 0,
+
+  // RWH Date style
+  // Full - ddd, MMM d, yyyy
+  // ISO - yyyy-MM-dd
+  //   "header.date.style": 0,
+
+  // RWH Time style
+  // 0 - 12 hours AM/PM
+  // 1 - 24 hours
+  "header.time.format": 0,
+
+  // RWH Date header include timezone info
+  "header.date.timezone": false,
+
+  // RWH Mail message
+  "clean.blockquote": true,
+  "clean.new.blockquote": false,
+  "clean.char.greaterthan": true,
+  "clean.only.new.quote.char": false,
+  "clean.pln.hdr.prefix": false,
+
+  // RWH Debug settings
+  "debug": false,
+}
+
+async function init () {
+  for (let [name, value] of Object.entries(PREF_DEFAULTS)) {
+    await browser.LegacyPrefs.setDefaultPref(`${PREF_PREFIX}${name}`, value);
+  }
+ 
   messenger.WindowListener.registerChromeUrl([
     ["content",  "replywithheader", "chrome/content/"],
     ["resource", "replywithheader", "skin/"]
@@ -25,4 +81,5 @@
 
   messenger.WindowListener.startListening();
 
-})()
+}
+init();
